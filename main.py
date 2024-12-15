b8752c25375cc5969a81541484521b9991fea6e1
ankitsharma5911 <ankitsharma450306@gmail.com>
2024-12-15 14:40:00 +0530

    data ingestion complete

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascience import logger
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
